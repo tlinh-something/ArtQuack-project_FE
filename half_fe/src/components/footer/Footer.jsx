c48182fd948df3ff2b169bc8502b94e921a55ed6
tlinh-something <144602540+tlinh-something@users.noreply.github.com>
2023-10-04 20:42:45 +0700

    Change some sentences

--- half_fe/src/components/footer/Footer.jsx
+++ half_fe/src/components/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
-import './footer.css';
+import '../footer/Footer.css';
 function Footer(){
     return (
 
