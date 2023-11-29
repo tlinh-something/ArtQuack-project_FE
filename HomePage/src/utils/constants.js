2161172e35a60fb6aa860f5b93333e1070e66d27
tlinh-something <144602540+tlinh-something@users.noreply.github.com>
2023-11-29 18:48:31 +0700

    Delete HomePage directory

--- HomePage/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const POTTERY = "pottery";     
-export const OIL = "oil";
-export const INLAY = "inlay";
-export const ENGRAVED_COPPER = "engraved_copper";
-export const SAND = "sand";
-export const SPRAY = "spray";
